1786d876bdb50af619f1e63cd364248996a55e88
Cleverttech <dzifatedeku@gmail.com>
2021-01-26 15:35:53 +0100

    added images and a bit of content

---
+++ app.js
@@ -0,0 +1,38 @@
+documentEventListener('DOMContentLoaded',() => {
+
+    //different cards
+    const cardArray =[
+        {
+            name:"apple",
+            img:'images/apple.png'
+        },
+        {
+            name:"pear",
+            img:'images/pear.png'  
+        },
+        {
+            name:"banana",
+            img:'images/banana.png'
+        },
+        {
+            name:"burger",
+            img:'images/burger.png'
+        },
+        {
+            name:"pizza",
+            img:'images/pizza.png'
+        },
+        {
+            name:"cherry",
+            img:'images/cherry.png'
+        },
+        {
+            name:"white",
+            img:'images/banana.png'
+        },
+        {
+            name:"transparent",
+            img:'images/transparent.png'
+        }
+    ]
+})
