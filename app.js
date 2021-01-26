164426feaa44dec0acdd672e05df6061dca00639
Cleverttech <dzifatedeku@gmail.com>
2021-01-26 16:53:47 +0100

    just updating

--- app.js
+++ app.js
@@ -1,38 +1,139 @@
-documentEventListener('DOMContentLoaded',() => {
+document.addEventListener('DOMContentLoaded', () => {
+
 
     //different cards
     const cardArray =[
         {
-            name:"apple",
+            name:'apple',
             img:'images/apple.png'
         },
         {
-            name:"pear",
-            img:'images/pear.png'  
+            name:'apple',
+            img:'images/apple.png'
+        },      
+        {
+            name:'pear',
+            img:'images/pear.png'
         },
         {
-            name:"banana",
+            name:'pear',
+            img:'images/pear.png'
+        },
+        {
+            name:'banana',
             img:'images/banana.png'
         },
         {
-            name:"burger",
+            name:'banana',
+            img:'images/banana.png'
+        },
+        {
+            name:'burger',
             img:'images/burger.png'
         },
         {
-            name:"pizza",
+            name:'burger',
+            img:'images/burger.png'
+        },
+        {
+            name:'pizza',
             img:'images/pizza.png'
         },
         {
-            name:"cherry",
+            name:'pizza',
+            img:'images/pizza.png'
+        },
+        {
+            name:'cherry',
             img:'images/cherry.png'
         },
         {
-            name:"white",
-            img:'images/banana.png'
-        },
-        {
-            name:"transparent",
-            img:'images/transparent.png'
+            name:'cherry',
+            img:'images/cherry.png'
         }
+        
     ]
+  
+    const grid = document.querySelector('.grid')
+
+    //creating my Board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++){
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/transparent.png')
+            card.setAttribute('data-id', i)
+            //card.addEventListener('click', flipcard)
+            grid.appendChild(card)
+        }
+    }
+    createBoard()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
