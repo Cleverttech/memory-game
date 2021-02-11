2002067b80fb03f6c5f3c2feb36ac20704f566e4
Cleverttech <dzifatedeku@gmail.com>
2021-02-11 18:18:48 +0100

    creating Array for the my images

--- app.js
+++ app.js
@@ -1,139 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-
     //different cards
     const cardArray =[
-        {
-            name:'apple',
+        { name:'apple',
             img:'images/apple.png'
         },
-        {
-            name:'apple',
+        {  name:'apple',
             img:'images/apple.png'
         },      
-        {
-            name:'pear',
+        { name:'pear',
             img:'images/pear.png'
         },
-        {
-            name:'pear',
+        {  name:'pear',
             img:'images/pear.png'
         },
-        {
-            name:'banana',
+        {   name:'banana',
             img:'images/banana.png'
         },
-        {
-            name:'banana',
+        {  name:'banana',
             img:'images/banana.png'
         },
-        {
-            name:'burger',
+        { name:'burger',
             img:'images/burger.png'
         },
-        {
-            name:'burger',
+        {   name:'burger',
             img:'images/burger.png'
         },
-        {
-            name:'pizza',
+        {  name:'pizza',
             img:'images/pizza.png'
         },
-        {
-            name:'pizza',
+        {  name:'pizza',
             img:'images/pizza.png'
         },
-        {
-            name:'cherry',
+        {  name:'cherry',
             img:'images/cherry.png'
         },
-        {
-            name:'cherry',
+        {  name:'cherry',
             img:'images/cherry.png'
-        }
-        
-       ]
-  
-    const grid = document.querySelector('.grid')
-
-    //creating my Board
-    function createBoard() {
-        for (let i = 0; i < cardArray.length; i++){
-            var card = document.createElement('img')
-            card.setAttribute('src', 'images/transparent.png')
-            card.setAttribute('data-id', i)
-            //card.addEventListener('click', flipcard)
-            grid.appendChild(card)
-        }
-    }
-    createBoard()
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }]
+      
+       
 })
