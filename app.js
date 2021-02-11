9aaf96c505ad0d3ff5ff35afdb32fca0a9388212
Cleverttech <dzifatedeku@gmail.com>
2021-02-11 18:20:14 +0100

    creating flip function-testing

--- app.js
+++ app.js
@@ -38,5 +38,38 @@ document.addEventListener('DOMContentLoaded', () => {
             img:'images/cherry.png'
         }]
       
+        cardArray.sort(() => 0.5 - Math.random())
+
+        const grid = document.querySelector('.grid')
+        const resultDisplay = document.querySelector('#result')
+        let cardsChosen = []
+        let cardsChosenId = []
+        let cardsWon = []
+      
+        //Board created here
+        function createBoard() {
+          for (let i = 0; i < cardArray.length; i++) {
+            const card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+          }
+        }
+      
+        //check for matches
+      
+        //flip  card
+        function flipCard() {
+          let cardId = this.getAttribute('data-id')
+          cardsChosen.push(cardArray[cardId].name)
+          cardsChosenId.push(cardId)
+          this.setAttribute('src', cardArray[cardId].img)
+          if (cardsChosen.length ===2) {
+            setTimeout(checkForMatch, 500)
+          }
+        }
+      
+        createBoard()
        
 })
